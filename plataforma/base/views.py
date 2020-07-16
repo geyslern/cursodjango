5a9e7ac98b28526de2d8da0de0123645a0b35a37
Geysler Niclevicz <geyslern@gmail.com>
2020-07-16 15:31:55 -0300

    Installed django-debug-toolbar
    It was installed django-debug-toolbar
    close #61

--- plataforma/base/views.py
+++ plataforma/base/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Hello Django. It works!')
+    return HttpResponse('<html><body>Hello Django. It works!</body></html>', content_type='text/html; charset=UTF-8')
--- plataforma/settings.py
+++ plataforma/settings.py
@@ -76,6 +76,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'plataforma.wsgi.application'
 
+# django-debug-toolbar configuration
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+    INTERNAL_IPS = config('INTERNAL_IPS', cast=Csv(), default='127.0.0.1')
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
