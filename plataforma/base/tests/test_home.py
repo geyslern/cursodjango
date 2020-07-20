96a5fe7752a358dd5e894a3e2fa19e767c122a55
Geysler Niclevicz <geyslern@gmail.com>
2020-07-20 18:34:49 -0400

    Page title test
    Test case for page title is created.
    close #74

--- plataforma/base/tests/test_home.py
+++ plataforma/base/tests/test_home.py
@@ -1,6 +1,12 @@
 from django.test import Client
+from plataforma.django_assertions import assert_contains
 
 
 def test_status_code(client: Client):
-    resp = client.get(path='/')
+    resp = client.get('/')
     assert 200 == resp.status_code
+
+
+def test_title(client: Client):
+    resp = client.get('/')
+    assert_contains(resp, '<title>Curso Plataforma Django</title>')
