2191318be875b4f307799dd607fb68bdb613d6b3
Geysler Niclevicz <geyslern@gmail.com>
2020-07-20 10:46:41 -0300

    Added Sentry support
    sentry-sdk is installed.
    close #67

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -14,6 +14,8 @@ import os
 import dj_database_url
 from decouple import Csv, config
 from functools import partial
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -170,3 +172,7 @@ if AWS_ACCESS_KEY_ID:  # pragma: no cover
 
     INSTALLED_APPS.append('s3_folder_storage')
     # INSTALLED_APPS.append('storages')
+
+SENTRY_DSN = config('SENTRY_DSN', default=None)
+if SENTRY_DSN:
+    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()], send_default_pii=True)
