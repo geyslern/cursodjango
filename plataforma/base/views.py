1986a3f9d683dcea2b7f8c99075a639f364d375a
Geysler Niclevicz <geyslern@gmail.com>
2020-07-20 17:45:51 -0400

    Front-end framework support
    Twitter bootstrap is installed.
    close #71

--- plataforma/base/views.py
+++ plataforma/base/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<html><body>Hello Django. It works!</body></html>', content_type='text/html; charset=UTF-8')
+    return render(request, 'base/home.html')
