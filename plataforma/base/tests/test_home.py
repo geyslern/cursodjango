0a57b83667cee5da758a0bb74ea4329c70444c18
Geysler Niclevicz <geyslern@gmail.com>
2020-10-31 22:45:05 -0300

    Named urls
    plataforma/urls.py edited to use named urls
    plataforma/base/urls.py was added to define base app urls.
    close #261

--- plataforma/base/tests/test_home.py
+++ plataforma/base/tests/test_home.py
@@ -1,12 +1,21 @@
-from django.test import Client
+import pytest
 from plataforma.django_assertions import assert_contains
+from django.urls import reverse
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
     assert 200 == resp.status_code
 
 
-def test_title(client: Client):
-    resp = client.get('/')
+def test_title(resp):
     assert_contains(resp, '<title>Curso Plataforma Django</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{ reverse("base:home") }">Python Pro</a>')
--- plataforma/urls.py
+++ plataforma/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from plataforma.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', include('plataforma.base.urls')),
 ]
 
 if settings.DEBUG:
