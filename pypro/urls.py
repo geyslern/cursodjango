a64b51f0d3df26814dbdc29f0a87922e8c5964f0
Geysler Niclevicz <1427080+geyslern@users.noreply.github.com>
2022-05-05 20:56:10 -0300

    Alterado idioma para pt-BR e timezone para America/Sao_Paulo
    close #26

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,4 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from pypro.base import views
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home, name="home")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home")
+]
