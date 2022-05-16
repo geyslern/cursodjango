91d004f64cb9375ba62c910fff1416a69f6d9463
Geysler Niclevicz <1427080+geyslern@users.noreply.github.com>
2022-05-15 22:49:38 -0300

    Utilização de url nomeada na página principal
    Criado teste para confirmar a existência de um link na navbar
    direcionado para a página princial.
    Alterado o template da página principal para gerar o link independente o
    endpoint definido para a página princial.
    
    close #48

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,12 +1,22 @@
 from django.test import Client
+from django.urls import reverse
+from pytest import fixture
 from pypro.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
-    assert resp.status_code == 200
+@fixture
+def response(client: Client):
+    resp = client.get(reverse('home'))
+    return resp
 
 
-def test_title(client: Client):
-    resp = client.get('/')
-    assert_contains(resp, '<title>Python Pro</title>')
+def test_status_code(response):
+    assert response.status_code == 200
+
+
+def test_title(response):
+    assert_contains(response, '<title>Python Pro</title>')
+
+
+def test_home_link(response):
+    assert_contains(response, f'href="{reverse("home")}">Python Pro</a>')
