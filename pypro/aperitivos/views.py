eba45c8bd0964e49f7e5ba1ba332143d3d4b7952
Geysler Niclevicz <1427080+geyslern@users.noreply.github.com>
2022-05-28 00:18:38 -0300

    Adicionado contexto de template na página de vídeos
    close #62

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '713071205?h=dbdac475f3'}
+    return render(request, 'aperitivos/video.html', context={'video': video})
