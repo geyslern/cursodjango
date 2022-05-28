00817877a49c83dd72886dbda5264932bed2e507
Geysler Niclevicz <1427080+geyslern@users.noreply.github.com>
2022-05-28 02:52:42 -0300

    Adicionado segundo vídeo aperitivo
    close #64

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -4,15 +4,25 @@ from django.test import Client
 from pypro.django_assertions import assert_contains
 
 @pytest.fixture
-def response(client:Client):
+def response_motivacao(client:Client):
     return client.get(reverse('aperitivos:video', args=('motivacao',)))
 
+@pytest.fixture
+def response_instalacao(client:Client):
+    return client.get(reverse('aperitivos:video', args=('instalacao-windows',)))
 
-def test_status_code(response):
-    assert response.status_code == 200
 
-def test_titulo_video(response):
-    assert_contains(response, 'Video Aperitivo: Motivação')
+def test_status_code(response_motivacao):
+    assert response_motivacao.status_code == 200
 
-def test_conteudo_video(response):
-    assert_contains(response, '<iframe src="https://player.vimeo.com/video/713071205?h=dbdac475f3"')
+def test_titulo_video_motivacao(response_motivacao):
+    assert_contains(response_motivacao, 'Video Aperitivo: Motivação')
+
+def test_conteudo_video_motivacao(response_motivacao):
+    assert_contains(response_motivacao, '<iframe src="https://player.vimeo.com/video/713071205"')
+
+def test_titulo_video_instalacao(response_instalacao):
+    assert_contains(response_instalacao, 'Video Aperitivo: Instalação no Windows')
+
+def test_conteudo_video_instalacao(response_instalacao):
+    assert_contains(response_instalacao, '<iframe src="https://player.vimeo.com/video/714670248"')
