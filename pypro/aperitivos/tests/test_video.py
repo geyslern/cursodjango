a7675cc97fb4f4dbb6fe1ef7754e0904334b1cf8
Geysler Niclevicz <1427080+geyslern@users.noreply.github.com>
2022-05-28 13:55:05 -0300

    Implementação índice de vídeos
    Criados índices dos vídeos aperitivos.
    
    close #68

--- pypro/aperitivos/tests/test_video.py
+++ pypro/aperitivos/tests/test_video.py
@@ -3,26 +3,32 @@ from django.urls import reverse
 from django.test import Client
 from pypro.django_assertions import assert_contains
 
-@pytest.fixture
-def response_motivacao(client:Client):
-    return client.get(reverse('aperitivos:video', args=('motivacao',)))
 
 @pytest.fixture
-def response_instalacao(client:Client):
-    return client.get(reverse('aperitivos:video', args=('instalacao-windows',)))
+def response_motivacao(client: Client):
+    return client.get(reverse("aperitivos:video", args=("motivacao",)))
+
+
+@pytest.fixture
+def response_instalacao(client: Client):
+    return client.get(reverse("aperitivos:video", args=("instalacao-windows",)))
 
 
 def test_status_code(response_motivacao):
     assert response_motivacao.status_code == 200
 
+
 def test_titulo_video_motivacao(response_motivacao):
-    assert_contains(response_motivacao, 'Video Aperitivo: Motivação')
+    assert_contains(response_motivacao, "Video Aperitivo: Motivação")
+
 
 def test_conteudo_video_motivacao(response_motivacao):
     assert_contains(response_motivacao, '<iframe src="https://player.vimeo.com/video/713071205"')
 
+
 def test_titulo_video_instalacao(response_instalacao):
-    assert_contains(response_instalacao, 'Video Aperitivo: Instalação no Windows')
+    assert_contains(response_instalacao, "Video Aperitivo: Instalação no Windows")
+
 
 def test_conteudo_video_instalacao(response_instalacao):
     assert_contains(response_instalacao, '<iframe src="https://player.vimeo.com/video/714670248"')
